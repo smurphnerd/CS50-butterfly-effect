7e4b302ba75ec13cf78435442c647d0e5e263753
therealSmurphy <smurphspam005@gmail.com>
2022-06-27 00:36:44 +1000

    Prototype for SQL to json in temp.py

--- temp.py
+++ temp.py
@@ -1,33 +1,38 @@
 import json
 from cs50 import SQL
-from flask import 
 
 db = SQL('sqlite:///butterfly-effect.db')
 
-json_object = {}
+root_object = {}
+root_data = db.execute('SELECT * FROM nodes WHERE user_id = ? AND id = ?', )[0]
+root_object['id'] = root_data['id']
+root_object['message'] = root_data['message']
+add_children(root_object)
 
+def add_children(root_object):
+    children = db.execute('SELECT * FROM children WHERE node_id = ?', )
+    if len(children) > 0:
+        root_object['children'] = []
+        for child in children:
+            child_object = {}
+            child_data = db.execute('SELECT * FROM nodes WHERE user_id = ? AND id = ?', )[0]
+            child_object['id'] = child_data['id']
+            child_object['message'] = child_data['message']
+            add_children(child_object)
+            root_object['children'].append(child_object)
+            return
+    else:
+        return
 
+"""
+define root attributes
 
-object = {
-    'id': 1,
-    'message': 'hello',
-    'children': [{
-        'id': 2,
-        'message': 'hello',
-        'children': [{
-            'id': 4,
-            'message': 'hello',
-            'children': []
-        }]
-    },
-    {
-        'id': 3,
-        'message': 'bye',
-        'children': []
-    }]
-    }
-
-with open('file.json', 'w') as f:
-    json.dump(object, f, indent=2)
-    print("The json file is created")
-
+Add children (root)
+    If root has children
+        root[children] = []
+        For each child, add attributes
+        Add children (child)
+        Append child to root children
+    else
+        return
+"""
